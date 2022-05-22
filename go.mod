2019358c28ca011d383092d36c3b82e08fdedef9
rajatgupta24 <rajat2411gupta@gmail.com>
2022-05-22 22:44:11 +0530

    add: go mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rajatgupta24/go-http-server.git
+
+go 1.17
--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
+
+type ToDoItem struct {
+	id     int
+	todo   string
+	isDone bool
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
@@ -19,13 +24,36 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World")
 }
 
-func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/hello", helloHandler)
+func addElement(todos []ToDoItem, todo ToDoItem) ToDoItem {
+	fmt.Println(todos, todo)
+}
 
-	fmt.Println("Server listening on PORT: 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
+func main() {
+	todos := []ToDoItem{
+		{
+			1,
+			"Need to test my code",
+			false,
+		},
+		{
+			2,
+			"Dockerise the app",
+			false,
+		},
 	}
+
+	// fileServer := http.FileServer(http.Dir("./static"))
+	// http.Handle("/", fileServer)
+	// http.HandleFunc("/hello", helloHandler)
+
+	// fmt.Println("Server listening on PORT: 8080")
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	todo := ToDoItem{3, "Deploy it on k3s", false}
+
+	addElement(todos, todo)
+
+	fmt.Println(todos)
 }
